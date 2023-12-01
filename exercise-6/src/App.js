535ae168b8a119db7341a36f75e65aaf44711914
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2023-12-01 16:57:12 +0100

    setup local

---
+++ exercise-6/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function App() {
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false
+})
+/**
+ * Challenge: Fill in the values in the markup
+ * using the properties of our state object above
+ * (Ignore `isFavorite` for now)
+ */
+
+function toggleFavorite() {
+    console.log("Toggle Favorite")
+}
+
+return (
+    <main>
+        <article className="card">
+            <img src="./images/user.png" className="card--image" alt=""/>
+            <div className="card--info">
+                <img 
+                    src={`../images/star-empty.png`} 
+                    className="card--favorite"
+                    onClick={toggleFavorite}
+                    alt=""
+                />
+                <h2 className="card--name">
+                    John Doe
+                </h2>
+                <p className="card--contact">+1 (719) 555-1212</p>
+                <p className="card--contact">itsmyrealname@example.com</p>
+            </div>
+            
+        </article>
+    </main>
+)
+}
+
+export default App;
