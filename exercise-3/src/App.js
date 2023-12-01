b695fc23543c1faa5462b827a53b66f284731b4a
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2023-12-01 14:29:21 +0100

    fix some react states

--- exercise-3/src/App.js
+++ exercise-3/src/App.js
@@ -1,14 +1,29 @@
 import React from "react"
 function App() {
-
+    /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
     const[number, counting] = React.useState(0)
-
+/* 
     function add(){
-        counting(number + 1)
+        counting(function(prevCounting) {
+            return prevCounting + 1;
+        })
     }
-
+ */
+    function add(){
+        counting(prevCounting => prevCounting + 1)
+    }
+    
     function subtract(){
-        counting(number - 1)
+        counting(prevCounting => prevCounting - 1)
     }
 
     return (
