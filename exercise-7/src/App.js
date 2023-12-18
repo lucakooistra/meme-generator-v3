4ef8bceecceecbf313981920dbb909829dc0c7b2
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2023-12-18 14:26:12 +0100

    setup local

--- exercise-7/src/App.js
+++ exercise-7/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import Body from "./components/Body";
 
 function App() {
   return (
-   <>
-   </>
+    <main>
+      <Header />
+      <Body />
+    </main>
   );
 }
 
