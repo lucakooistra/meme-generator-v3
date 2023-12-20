27fd47b4bda376a27d6d6d6259277403192f4d40
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2023-12-20 14:34:28 +0100

    create styling based on the data

--- exercise-8/src/App.js
+++ exercise-8/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react'
-import boxes from "./boxes"
+import boxes from "./data/boxes"
+import Box from "./components/Box"
 
 function App(props) {
 
     const [squares, setSquares] = React.useState(boxes)
     
-    const styles = {
-      backgroundColor:  props.darkMode ? "#222222" : "#cccccc"
-    }
-
-    console.log(styles)
-
     const squareElements = squares.map(square => {
       return (
-        <div style={styles} className="box" key={square.id}></div>
+        <Box 
+        key={square.id}
+        on={square.on}
+        />
       )
     })
+
+        /**
+     * Challenge part 2:
+     * 1. Create a separate component called "Box" and
+     *    replace the `div` above with our <Box /> components
+     * 2. Pass the Box component a prop called `on` with the
+     *    value of the same name from the `boxes` objects
+     * 3. In the Box component, apply dynamic styles to determine
+     *    the backgroundColor of the box. If it's `on`, set the
+     *    backgroundColor to "#222222". If off, set it to "none"
+     */
   return (
     <main>
       {squareElements}
