0489423895488e2f80b8d0081483dc87604cd2a3
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2023-12-20 15:59:25 +0100

    make a clicked function

--- exercise-8/src/App.js
+++ exercise-8/src/App.js
@@ -4,27 +4,20 @@ import Box from "./components/Box"
 
 function App(props) {
 
+  function toggle() {
+    console.log("Clicked!")
+  }
+
     const [squares, setSquares] = React.useState(boxes)
     
-    const squareElements = squares.map(square => {
-      return (
-        <Box 
-        key={square.id}
-        on={square.on}
+    const squareElements = squares.map(square => (
+        <Box
+          handleClick={toggle} 
+          key={square.id}
+          on={square.on}
         />
-      )
-    })
-
-        /**
-     * Challenge part 2:
-     * 1. Create a separate component called "Box" and
-     *    replace the `div` above with our <Box /> components
-     * 2. Pass the Box component a prop called `on` with the
-     *    value of the same name from the `boxes` objects
-     * 3. In the Box component, apply dynamic styles to determine
-     *    the backgroundColor of the box. If it's `on`, set the
-     *    backgroundColor to "#222222". If off, set it to "none"
-     */
+      ))
+      
   return (
     <main>
       {squareElements}
