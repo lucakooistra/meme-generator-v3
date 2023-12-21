cd9de7bb5c5d02ae372a51ffdd7c1c6102614c63
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2023-12-21 15:51:50 +0100

    use the .map function to check array on same id

--- exercise-8/src/App.js
+++ exercise-8/src/App.js
@@ -7,26 +7,17 @@ function App(props) {
 
   function toggle(id) {
     setSquares(prevSquares => {
-      const newSquares = []
-      for(let i = 0; i < prevSquares.length; i++) {
-        const currentSquare = prevSquares[i]
-        if(currentSquare.id === id) {
-          const updatedSquare = {
-            ...currentSquare,
-            on: !currentSquare.on
-          }
-          newSquares.push(updatedSquare)
-        } 
-        else {
-          newSquares.push(currentSquare)
-        }
-      }
-      return newSquares
+      return prevSquares.map((square) => {
+        return square.id === id ? {...square, on: !square.on} : square
+      })
     })
   }
 
   const squareElements = squares.map((square) => (
-    <Box key={square.id} id={square.id} on={square.on} handleClick={toggle} />
+    <Box
+      key={square.id}
+      on={square.on}
+      handleClick={() => toggle(square.id)} />
   ));
 
   return <main>{squareElements}</main>;
--- exercise-8/src/components/Box.jsx
+++ exercise-8/src/components/Box.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 
 export default function Box(props) {
-    const styles = {
-    backgroundColor:props.on ? "#222222" : "transparent"
-    }
+  const styles = {
+    backgroundColor: props.on ? "#222222" : "transparent",
+  };
 
-    return(
-        <div 
-            style={styles} 
-            className="box"
-            onClick={()=>
-                props.handleClick(props.id)
-            }
-        >
-
-        </div>
-    )
+  return <div 
+  style={styles} 
+  className="box" 
+  onClick={props.handleClick}></div>;
 }
