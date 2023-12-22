b3d67f6b56ba4b9d31154ec914060c92d28011f4
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2023-12-22 13:54:25 +0100

    create toggle function for punchline

--- exercise-9/src/components/Joke.jsx
+++ exercise-9/src/components/Joke.jsx
@@ -1,15 +1,18 @@
 import React from "react"
 
 export default function Joke(props) {
-    /**
-     * Challenge:
-     * - Create state `isShown` (boolean, default to `false`)
-     * - Add a button that toggles the value back and forth
-     */
+
+    const [isShown, setIsShown] = React.useState(false)
+
+    function toggle(){
+        setIsShown(prevIsShown => !prevIsShown)
+    }
+
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            <p>{props.punchline}</p>
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggle}>{isShown ? " Hide" : "Show"} Punchline</button>
             <hr />
         </div>
     )
