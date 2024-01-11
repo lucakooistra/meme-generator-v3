740c94221e38ce590b21328a36fb55ca680f9647
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2024-01-11 16:46:58 +0100

    learn event targets in react

--- exercise-11/src/App.js
+++ exercise-11/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 function App() {
+  const [firstName, setFirstName] = React.useState("")
+  
+  console.log(firstName)
+  
+  function handleChange(event){
+    setFirstName(event.target.value)
+  }
+
   return (
     <form>
-      <input type="text" placeholder="First Name" />
+      <input 
+        type="text" 
+        placeholder="First Name"
+        onChange={handleChange}
+      />
     </form>
   );
 }
