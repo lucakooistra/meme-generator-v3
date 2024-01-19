eacae6b102b7072329cc03be6efceb0a92480230
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2024-01-19 16:27:04 +0100

    make forms a controlled input

--- exercise-11/src/App.js
+++ exercise-11/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function App() {
-  const [firstName, setFirstName] = React.useState("")
+  const [formData, setFormData] = React.useState(
+    {firstName: "", lastName: "", email: ""}
+    )
   
-  console.log(firstName)
+    console.log(formData)
   
   function handleChange(event){
-    setFirstName(event.target.value)
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value
+      }
+    })
   }
 
   return (
@@ -15,6 +22,22 @@ function App() {
         type="text" 
         placeholder="First Name"
         onChange={handleChange}
+        name="firstName"
+        value={formData.firstName}
+      />
+      <input 
+        type="text" 
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+      <input 
+        type="email" 
+        placeholder="email"
+        onChange={handleChange}
+        name="email"
+        value={formData.email}
       />
     </form>
   );
